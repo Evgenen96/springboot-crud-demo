6fc7ddaa01f590006c51900bb5cea3b53bfa70e6
Eugene Nikonov <nikonovse96@gmail.com>
2020-10-22 19:30:50 +0400

    spring data rest api usage

--- src/main/java/ru/cofeok/springboot/crud/demo/dao/EmployeeRepository.java
+++ src/main/java/ru/cofeok/springboot/crud/demo/dao/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package ru.cofeok.springboot.crud.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.cofeok.springboot.crud.demo.entity.Employee;
 
+@RepositoryRestResource(path = "clients")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
